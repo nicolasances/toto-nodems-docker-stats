62d1018972b896b94ffab7401c60106507e9f2cf
nicolas <nicolas.matteazzi@gmail.com>
2018-02-03 19:36:17 +0100

    Integration with monitoring api calls

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var Promise = require('promise');
 var bodyParser = require("body-parser");
+var logger = require('toto-apimon-events')
 
 var getDockerStats = require('./dlg/GetDockerStatsDelegate');
 
@@ -16,7 +17,7 @@ app.use(bodyParser.json());
 app.use(express.static('/app'));
 
 app.get('/', function(req, res) {res.send({api: 'docker-stats', status: 'running'});});
-app.get('/stats', function(req, res) {getDockerStats.getDockerStats().then(function(result) {res.send(result);});});
+app.get('/stats', function(req, res) {logger.apiCalled('docker-stats', '/stats', 'GET', req.query, req.params, req.body); getDockerStats.getDockerStats().then(function(result) {res.send(result);});});
 
 app.listen(8080, function() {
   console.log('Docker Stats Microservice up and running');

54d29ef8131163005c5ddf387f92edc78984c302
nicolas <nicolas.matteazzi@gmail.com>
2018-08-26 11:15:36 +0200

    Updated api name

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(express.static('/app'));
 
 app.get('/', function(req, res) {res.send({api: apiName, status: 'running'});});
-app.get('/stats', function(req, res) {logger.apiCalled('docker-stats', '/stats', 'GET', req.query, req.params, req.body); getDockerStats.getDockerStats().then(function(result) {res.send(result);});});
+app.get('/stats', function(req, res) {logger.apiCalled(apiName, '/stats', 'GET', req.query, req.params, req.body); getDockerStats.getDockerStats().then(function(result) {res.send(result);});});
 
 app.listen(8080, function() {
   console.log('Docker Stats Microservice up and running');

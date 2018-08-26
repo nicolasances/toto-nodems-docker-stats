af61204d8fafe4e8b2b12b6f1f39c4d88cbca140
nicolas <nicolas.matteazzi@gmail.com>
2018-08-26 11:01:29 +0200

    Authorization header accepted

--- index.js
+++ index.js
@@ -5,18 +5,20 @@ var logger = require('toto-apimon-events')
 
 var getDockerStats = require('./dlg/GetDockerStatsDelegate');
 
+var apiName = 'docker-stats';
+
 var app = express();
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, GoogleIdToken");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST, DELETE");
   next();
 });
 app.use(bodyParser.json());
 app.use(express.static('/app'));
 
-app.get('/', function(req, res) {res.send({api: 'docker-stats', status: 'running'});});
+app.get('/', function(req, res) {res.send({api: apiName, status: 'running'});});
 app.get('/stats', function(req, res) {logger.apiCalled('docker-stats', '/stats', 'GET', req.query, req.params, req.body); getDockerStats.getDockerStats().then(function(result) {res.send(result);});});
 
 app.listen(8080, function() {
